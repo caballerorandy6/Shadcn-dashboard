fa2ef2daa26d0b80f4d9b78d6b49718aa46c6bcb
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2024-10-11 00:15:55 -0500

    form section done

--- src/app/dashboard/command/page.tsx
+++ src/app/dashboard/command/page.tsx
@@ -1,72 +1,77 @@
-import React from "react";
+"use client";
 
-export default function page() {
-  return <div></div>;
+import { useState, useEffect } from "react";
+
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+
+export default function Page() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <div>
+      <CommandDialog open={open} onOpenChange={(value) => setOpen(value)}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem onSelect={() => console.log("Calendar")}>
+              Calendar
+            </CommandItem>
+            <CommandItem onSelect={() => console.log("Search Emoji")}>
+              Search Emoji
+            </CommandItem>
+            <CommandItem onSelect={() => console.log("Calculator")}>
+              Calculator
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem onSelect={() => console.log("Profile")}>
+              Profile
+            </CommandItem>
+            <CommandItem onSelect={() => console.log("Billing")}>
+              Billing
+            </CommandItem>
+            <CommandItem onSelect={() => console.log("Settings")}>
+              Settings
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+
+      <div className="mt-10 h-[200px] flex justify-center items-center">
+        <p className="text-sm text-muted-foreground">
+          Press{" "}
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            <span className="text-xs">⌘</span>J
+          </kbd>{" "}
+          or{" "}
+          <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            <span className="text-xs">CTRL +</span>J
+          </kbd>
+        </p>
+      </div>
+    </div>
+  );
 }
-
-// "use client";
-
-// import { useState, useEffect } from "react";
-
-// import {
-//   Command,
-//   CommandDialog,
-//   CommandEmpty,
-//   CommandGroup,
-//   CommandInput,
-//   CommandItem,
-//   CommandList,
-//   CommandSeparator,
-//   CommandShortcut,
-// } from "@/components/ui/command";
-
-// export default function Page() {
-//   const [open, setOpen] = useState(false);
-
-//   useEffect(() => {
-//     const onKeyDown = (e: KeyboardEvent) => {
-//       if (e.key === "j" && (e.metaKey || e.ctrlKey)) {
-//         e.preventDefault();
-//         setOpen((open) => !open);
-//       }
-//     };
-
-//     document.addEventListener("keydown", down);
-//     return () => document.removeEventListener("keydown", down);
-//   }, []);
-
-//   return (
-//     <div>
-//       <CommandDialog open={open} onOpenChange={setOpen}>
-//         <CommandInput placeholder="Type a command or search..." />
-//         <CommandList>
-//           <CommandEmpty>No results found.</CommandEmpty>
-//           <CommandGroup heading="Suggestions">
-//             <CommandItem>Calendar</CommandItem>
-//             <CommandItem>Search Emoji</CommandItem>
-//             <CommandItem>Calculator</CommandItem>
-//           </CommandGroup>
-//           <CommandSeparator />
-//           <CommandGroup heading="Settings">
-//             <CommandItem>Profile</CommandItem>
-//             <CommandItem>Billing</CommandItem>
-//             <CommandItem>Settings</CommandItem>
-//           </CommandGroup>
-//         </CommandList>
-//       </CommandDialog>
-
-//       <div className="mt-20 h-[200px] flex justify-center items-center">
-//         <p className="text-sm text-muted-foreground">
-//           Press{" "}
-//           <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-//             <span className="text-xs">⌘</span>J
-//           </kbd>{" "}
-//           or{" "}
-//           <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
-//             <span className="text-xs">CTRL</span> + J
-//           </kbd>
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -20,6 +20,7 @@ const links = [
   { name: "carousel", href: "carousel" },
   { name: "checkbox", href: "checkbox" },
   { name: "command", href: "command" },
+  { name: "form", href: "form" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
